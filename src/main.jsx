0b3d151344f3dc28c5785c66ba1a7e3c31d3be51
SK-Washif <washiffishal@gmail.com>
2024-10-22 18:30:35 +0600

    cart details and order items display and remove added

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,46 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Shop from './components/Shop/Shop.jsx';
+import Home from './components/Layout/Home.jsx';
+
+import Inventory from './components/Inventory/Inventory.jsx';
+import Login from './components/Login/Login.jsx';
+import Orders from './components/Orders/Orders.jsx';
+import cartProductsLoader from './loaders/cartProductsLoader.js';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home></Home>,
+    children:[
+      {
+        path: '/',
+        element: <Shop></Shop>
+      },
+      {
+        path: 'orders',
+        element: <Orders></Orders>,
+        loader: cartProductsLoader
+      },
+      {
+        path: 'inventory',
+        element: <Inventory></Inventory>
+      },
+      {
+        path: 'login',
+        element: <Login></Login>
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
